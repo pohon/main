2c8d414b7a9e29430cf5ee4763edfb2040959a4b
Sullivan Osmond <osmond@tiket.com>
2018-01-04 14:43:39 +0700

    add basic bootstrap

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 //NavLink support styling
+//NavLink must be placed under component <BrowserRouter>
 import { NavLink } from 'react-router-dom';
 
+
+
 const Header = (props) => (
-  <ul>
-    <li><NavLink activeClassName="active" exact 
-          to="/">Home</NavLink></li>
-    <li><NavLink activeClassName="active" 
-          to="/dashboard">Dashboard</NavLink></li>
-    <li><NavLink activeClassName="active"  
-          to="/cart">Cart</NavLink></li>
-  </ul>
+      <header>
+            <div class="container">
+                  <ul className="row">
+                        <li><NavLink activeClassName="active" exact 
+                              to="/">Home</NavLink></li>
+                        <li><NavLink activeClassName="active" 
+                              to="/dashboard">Dashboard</NavLink></li>
+                        <li><NavLink activeClassName="active"  
+                              to="/cart">Cart</NavLink></li>
+                  </ul>
+            </div>
+      </header>
+  
 );
 
 export default Header;
